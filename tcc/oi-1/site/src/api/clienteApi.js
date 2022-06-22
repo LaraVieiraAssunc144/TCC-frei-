f98f7c80af389d0d61b8e935ec61285122474605
Lara Assunção Vieira <assuncaolara96@gmail.com>
2022-06-21 21:54:40 -0300

    aaa

--- tcc/oi-1/site/src/api/clienteApi.js
+++ tcc/oi-1/site/src/api/clienteApi.js
@@ -17,3 +17,26 @@ export async function cadastrarCliente(nome,cpf,nascimento,horario,data,telefone
     })
     return resposta.data;
 }
+
+export async function alterarCliente(id,nome,cpf,nascimento,horario,data,telefone,situacao){
+    const resposta =await api.put(`/cliente/${id}`,{
+        nome:nome,
+        cpf:cpf,
+        nascimento:nascimento,
+        horario:horario,
+        data:data,
+        telefone:telefone,
+        situacao:situacao
+    })
+    return resposta.data;
+}
+
+export async function listarTodosClientes(){
+    const resposta =await api.get('/filme');
+    resposta.data;
+}
+
+export async function buscarClientesporCPF(cpf){
+    const resposta =await api.get(`/filme/busca?cpf=${cpf}`);
+    resposta.data;
+}
--- tcc/oi-1/site/src/pages/1.Login/index.js
+++ tcc/oi-1/site/src/pages/1.Login/index.js
@@ -1,6 +1,7 @@
 import { login } from '../../api/usuarioApi'
 import {useNavigate} from 'react-router-dom'
 
+
 import storage from 'local-storage'
 import LoadingBar from 'react top-loading-bar'
 import{useState,useRef, useEffect} from 'react'
@@ -17,6 +18,7 @@ export default function Index() {
     const[senha,setSenha]= useState('');
     const[erro,setErro]= useState('');
     const[carregando,setCarregando]=useState(false);
+    
 
     const navigate= useNavigate();
     const ref= useRef();
